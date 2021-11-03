af0b5dfcfb77ac6a3d8953aec44323d896e35a84
Sachin Yadav <88329785+designed2code@users.noreply.github.com>
2021-11-03 23:12:05 +0530

    Updated twoSum Solution

--- twosum.js
+++ twosum.js
@@ -1,25 +1,23 @@
-// Two Sum - Submission Detail - LeetCode
+// Two Sum - Submission Detail - LeetCode https://leetcode.com/problems/two-sum/
 // https://levelup.gitconnected.com/how-to-solve-two-sum-in-javascript-d1ebd9dfd3d3
 
-// 1. Declare an empty object (hash map)
-// 2. Loop over the array
-// 3. Store the current element of the array
-// 4. Check if the target minus current element value is present in the hash map
-// 5. If present then return the the target minus current element value and current element
-// 6. Else push the current element in the hash map
-// 7. Return empty array if no numbers add up to the target value.
+//1.Declare a map, 
+//2.standard for loop, 
+//3.calculate the complement by target - nums[i]
+//4.if complement is present in map then return current index i and map.get(complement)
+//5.finally if complement is not present in map then add that element into map by map.set(nums[i], i)
 
-const twoSum = function(nums, target) {
-    const hash = {}; // Stores seen numbers: {seenNumber: indexItOccurred}
-  
-    for (let i = 0; i < nums.length; i++) { // loop through all numbers
-      const n = nums[i]; // grab the current number `n`.
-      if (hash[target - n] !== undefined) { // check if the number we need to add to `n` to reach our target has been seen:
-        return [hash[target - n], i]; // grab the index of the seen number, and the index of the current number
-      }
-      hash[n] = i; // update our hash to include the. number we just saw along with its index.
+const twoSum = (nums, target) => {
+    let map = new Map()
+    let i;
+    let length = nums.length
+    for(i=0;i<length;i++){
+        let complement = target - nums[i]
+        if(map.has(complement)){
+            return [i,map.get(complement)]
+        }
+        map.set(nums[i], i)
     }
-    return []; // If no numbers add up to equal the `target`, we can return an empty array
-  }
+}
   
   console.log(twoSum([2,7,11,15], 9)); // [1, 2]
