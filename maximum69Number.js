637398b3dfc24fc030a7097705eadc9abbdcfc04
Sachin Yadav <88329785+designed2code@users.noreply.github.com>
2022-03-15 23:03:42 +0530

    Create maximum69Number.js

---
+++ maximum69Number.js
@@ -0,0 +1,27 @@
+Question https://leetcode.com/problems/maximum-69-number/
+
+Example 1:
+
+Input: num = 9669
+Output: 9969
+Explanation: 
+Changing the first digit results in 6669.
+Changing the second digit results in 9969.
+Changing the third digit results in 9699.
+Changing the fourth digit results in 9666.
+The maximum number is 9969.
+Example 2:
+
+Input: num = 9996
+Output: 9999
+Explanation: Changing the last digit 6 to 9 results in the maximum number.
+Example 3:
+
+Input: num = 9999
+Output: 9999
+Explanation: It is better not to apply any change.
+
+// CODE
+const maximum69Number = (num) => {
+  return Number(num.toString().replace('6','9'))
+}
